7f9724ebfd1a46ee11a5bab7ff9060d0ea64c87f
jsonaguas <jaguas43@gmail.com>
2024-09-11 22:38:57 -0500

    upload

---
+++ Problem1.py
@@ -0,0 +1,11 @@
+#Task 1
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+sorted_grades = sorted(grades, reverse=True)
+print(sorted_grades)
+
+#Task 2
+grades_total = sum(grades) 
+grades_count = len(grades) 
+score_average = grades_total / grades_count
+print(score_average)
+
---
+++ Problem2.py
@@ -0,0 +1,8 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice is submitted and attended")
+else:
+    pass
+
